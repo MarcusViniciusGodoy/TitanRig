9cd71f7ff4c01918c8e84c690292bd12586a02d9
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-28 13:28:39 -0300

    Created class Category and relation ManyToMany

--- src/main/java/com/titanrig/titanrig/entities/Category.java
+++ src/main/java/com/titanrig/titanrig/entities/Category.java
@@ -4,13 +4,10 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -23,8 +20,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tb_product")
-public class Product implements Serializable{
+@Table(name = "tb_category")
+public class Category implements Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -32,13 +29,6 @@ public class Product implements Serializable{
     private Long id;
     private String name;
 
-    @Column(columnDefinition = "TEXT")
-    private String description;
-    private Double price;
-
-    @ManyToMany
-    @JoinTable(name = "tb_product_category",
-         joinColumns = @JoinColumn(name = "product_id"),
-         inverseJoinColumns = @JoinColumn(name = "category_id"))
-    Set<Category> categories = new HashSet<>();
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 }
