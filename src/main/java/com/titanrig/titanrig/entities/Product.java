b67f624712fc3a8f80a3be248fcacc25c64c800e
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-28 13:22:32 -0300

    Created class Product and tested

---
+++ src/main/java/com/titanrig/titanrig/entities/Product.java
@@ -0,0 +1,32 @@
+package com.titanrig.titanrig.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+
+}
