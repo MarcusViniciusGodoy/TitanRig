d52c4c8b826fe9a5c40f6c1e9495ed50c126f7cf
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-28 13:53:09 -0300

    Created relation ManyToMany user_role and administrator_role

--- src/main/java/com/titanrig/titanrig/entities/Administrator.java
+++ src/main/java/com/titanrig/titanrig/entities/Administrator.java
@@ -1,9 +1,16 @@
 package com.titanrig.titanrig.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,4 +33,10 @@ public class Administrator implements Person{
     private String phone;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_administrator_role",
+         joinColumns = @JoinColumn(name = "administrator_id"),
+         inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<Role> roles = new HashSet<>();
 }
--- src/main/java/com/titanrig/titanrig/entities/User.java
+++ src/main/java/com/titanrig/titanrig/entities/User.java
@@ -1,9 +1,16 @@
 package com.titanrig.titanrig.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,4 +34,10 @@ public class User implements Person{
     private String email;
     private String password;
     private String cpf;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+         joinColumns = @JoinColumn(name = "user_id"),
+         inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<Role> roles = new HashSet<>();
 }
