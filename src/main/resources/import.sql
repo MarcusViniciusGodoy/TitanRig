273c9d4e92b8fd5442207caae4de2cc254705781
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-29 11:09:42 -0300

    Insert into H2 info tests

---
+++ src/main/resources/import.sql
@@ -0,0 +1,27 @@
+INSERT INTO tb_user (name, phone, email, password, cpf) VALUES ('Alex', '999998888', 'alex@gmail.com', '123456', '111111111');
+INSERT INTO tb_user (name, phone, email, password, cpf) VALUES ('Maria', '777776666', 'maria@gmail.com', '7891011', '222222222');
+
+INSERT INTO tb_administrator (name, phone, email, password) VALUES ('Leticia', '555552222', 'leticia@gmail.com', 'leticia1');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_administrator_role (administrator_id, role_id) VALUES (1, 2);
+
+INSERT INTO tb_category (name) VALUES ('Placas mães');
+INSERT INTO tb_category (name) VALUES ('Memorias');
+INSERT INTO tb_category (name) VALUES ('Gabinetes');
+
+INSERT INTO tb_product (name, description, price) VALUES ('Aorus', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 950.0);
+INSERT INTO tb_product (name, description, price) VALUES ('TUF gamer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 1050.0);
+INSERT INTO tb_product (name, description, price) VALUES ('MAtrix', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 450.0);
+INSERT INTO tb_product (name, description, price) VALUES ('Tridant', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 765.50);
+INSERT INTO tb_product (name, description, price) VALUES ('Corsair', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 697.85);
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (5, 2);
