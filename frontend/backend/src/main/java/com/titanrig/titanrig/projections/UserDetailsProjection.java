fe1f95227f50b1dfdf7cabbbddf68dd375bef734
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-05-02 17:27:33 -0300

    Atualizando

---
+++ frontend/backend/src/main/java/com/titanrig/titanrig/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.titanrig.titanrig.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    HeaderComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'TitanRig';
+}
