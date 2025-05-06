c7a3151d816ad8f05e4f1f65ed4ebd2f15ed4841
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-05-06 11:48:11 -0300

    Delete product if role_admin

--- backend/src/main/java/com/titanrig/titanrig/services/ProductService.java
+++ backend/src/main/java/com/titanrig/titanrig/services/ProductService.java
@@ -3,9 +3,11 @@ package com.titanrig.titanrig.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.titanrig.titanrig.dto.CategoryDTO;
@@ -14,6 +16,7 @@ import com.titanrig.titanrig.entities.Category;
 import com.titanrig.titanrig.entities.Product;
 import com.titanrig.titanrig.exceptions.ResourceNotFoundException;
 import com.titanrig.titanrig.repositories.ProductRepository;
+import com.titanrig.titanrig.services.exceptions.DatabaseException;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -53,6 +56,19 @@ public class ProductService {
             return new ProductDTO(entity);
         } catch (EntityNotFoundException e){
             throw new ResourceNotFoundException("Recurso não encontrado.");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+	    if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado.");
+        }
+        try {
+            repository.deleteById(id);    		
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial.");
         }
     }
 
