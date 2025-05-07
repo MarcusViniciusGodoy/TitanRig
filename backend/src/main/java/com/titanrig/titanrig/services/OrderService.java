68f050f47cba8c4cb8fef57d5b19c98c47ed898d
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-05-07 10:10:49 -0300

    Created and tested insert orders

--- backend/src/main/java/com/titanrig/titanrig/services/OrderService.java
+++ backend/src/main/java/com/titanrig/titanrig/services/OrderService.java
@@ -1,11 +1,18 @@
 package com.titanrig.titanrig.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.titanrig.titanrig.dto.OrderDTO;
+import com.titanrig.titanrig.dto.OrderItemDTO;
 import com.titanrig.titanrig.entities.Order;
+import com.titanrig.titanrig.entities.OrderItem;
+import com.titanrig.titanrig.entities.OrderStatus;
+import com.titanrig.titanrig.entities.Product;
+import com.titanrig.titanrig.entities.User;
 import com.titanrig.titanrig.repositories.OrderItemRepository;
 import com.titanrig.titanrig.repositories.OrderRepository;
 import com.titanrig.titanrig.repositories.ProductRepository;
@@ -35,5 +42,28 @@ public class OrderService {
                 () -> new ResourceNotFoundException("Recurso não encontrado"));
         authService.validateSelfOrAdmin(order.getClient().getId());
         return new OrderDTO(order);
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto){
+
+        Order order = new Order();
+
+        order.setMoment(Instant.now());
+        order.setStatus(OrderStatus.WAITING_PAYMENT);
+
+        User user = userService.authenticated();
+        order.setClient(user);
+
+        for (OrderItemDTO itemDTO : dto.getItems()){
+            Product product = productRepository.getReferenceById(itemDTO.getProductId());
+            OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), product.getPrice());
+            order.getItems().add(item);
+        }
+
+        repository.save(order);
+        orderItemRepository.saveAll(order.getItems());
+
+        return new OrderDTO();
     }
 }
