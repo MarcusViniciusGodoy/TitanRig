bbed5373945f21df5f8758f1810cbba496c3b799
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-05-17 16:48:06 -0300

    Created Product and CategoryControllerIT no tested

---
+++ backend/src/test/java/com/titanrig/titanrig/controllers/ProductControllerIT.java
@@ -0,0 +1,44 @@
+package com.titanrig.titanrig.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.titanrig.titanrig.tests.TokenUtil;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	private String operatorUsername;
+	private String operatorPassword;
+	private String adminUsername;
+	private String adminPassword;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		operatorUsername = "ana@gmail.com";
+		operatorPassword = "123456";
+		adminUsername = "bob@gmail.com";
+		adminPassword = "123456";
+	}
+}
+
