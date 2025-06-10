77df3e69fee5e99bd367ca93db8c537e9025de85
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-10 09:39:48 -0300

    Review Controller

--- backend/src/main/java/com/titanrig/titanrig/controller/ReviewController.java
+++ backend/src/main/java/com/titanrig/titanrig/controller/ReviewController.java
@@ -1,5 +1,32 @@
 package com.titanrig.titanrig.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.titanrig.titanrig.dto.ReviewDTO;
+import com.titanrig.titanrig.dto.ReviewInsertDTO;
+import com.titanrig.titanrig.services.ReviewService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/reviews")
 public class ReviewController {
 
+    @Autowired
+    private ReviewService service;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    public ResponseEntity<ReviewDTO> insert(@Valid @RequestBody ReviewInsertDTO dto) {
+        ReviewDTO result = service.insert(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
 }
+
