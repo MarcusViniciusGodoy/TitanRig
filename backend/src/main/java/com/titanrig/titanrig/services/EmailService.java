a6ff8fd9cc299f9f1777eed8ca786fadfc698bc5
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-12 08:29:14 -0300

    User authenticated

--- backend/src/main/java/com/titanrig/titanrig/services/EmailService.java
+++ backend/src/main/java/com/titanrig/titanrig/services/EmailService.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.titanrig.titanrig.entities.User;
+import com.titanrig.titanrig.repositories.UserRepository;
 import com.titanrig.titanrig.services.exceptions.EmailException;
 
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @Service
 public class EmailService {
@@ -18,6 +24,9 @@ public class EmailService {
 	
     @Autowired
     private JavaMailSender emailSender;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public void sendEmail(String to, String subject, String body) {
         try{
@@ -31,5 +40,16 @@ public class EmailService {
         catch (MailException e){
         	throw new EmailException("Failed to send email");
         } 
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return userRepository.findByEmail(username);
+        }catch (Exception e) {
+            throw new UsernameNotFoundException("Invalid user");
+        }
     }
 }
