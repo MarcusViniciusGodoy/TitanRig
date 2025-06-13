0e2c37251c5abd1afa5fd3a1c63de36f3aac8554
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-13 15:16:02 -0300

    Recover Token

--- backend/src/main/java/com/titanrig/titanrig/services/AuthService.java
+++ backend/src/main/java/com/titanrig/titanrig/services/AuthService.java
@@ -1,5 +1,8 @@
 package com.titanrig.titanrig.services;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -7,10 +10,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.titanrig.titanrig.dto.EmailDTO;
+import com.titanrig.titanrig.entities.PasswordRecover;
 import com.titanrig.titanrig.entities.User;
+import com.titanrig.titanrig.repositories.PasswordRecoverRepository;
 import com.titanrig.titanrig.repositories.UserRepository;
 import com.titanrig.titanrig.services.exceptions.ForbiddenException;
+import com.titanrig.titanrig.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class AuthService {
@@ -22,16 +30,44 @@ public class AuthService {
     private Long recoverUri;
 
     @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
+
+    @Autowired
     private UserService userService;
 
     @Autowired
 	private UserRepository userRepository;
+
+    @Autowired
+    private EmailService emailService;
 
     public void validateSelfOrAdmin(long userId){
         User me = userService.authenticated();
         if (!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)){
             throw new ForbiddenException("Access denied");
         }
+    }
+
+    @Transactional
+    public void createRecoverToken(EmailDTO body) {
+        User user = userRepository.findByEmail(body.getEmail());
+
+        if (user == null){
+            throw new ResourceNotFoundException("Email não encontrado");
+        }
+
+        String token = UUID.randomUUID().toString();
+
+        PasswordRecover entity = new PasswordRecover();
+        entity.setEmail(body.getEmail());
+        entity.setToken(token);
+        entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60L));
+        entity = passwordRecoverRepository.save(entity);
+
+        String text = "Acesse o link para redefinir uma nova senha\n\n"
+        + recoverUri + token + ". Validade de " + tokenMinutes + " minutos";
+
+        emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
     }
 
     protected User authenticated() {
