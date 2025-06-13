fe346ca9d5c0f744f51b869dc23d41f66e50ce70
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-13 15:38:22 -0300

    Save password

--- backend/src/main/java/com/titanrig/titanrig/services/AuthService.java
+++ backend/src/main/java/com/titanrig/titanrig/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.titanrig.titanrig.services;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.titanrig.titanrig.dto.EmailDTO;
+import com.titanrig.titanrig.dto.NewPasswordDTO;
 import com.titanrig.titanrig.entities.PasswordRecover;
 import com.titanrig.titanrig.entities.User;
 import com.titanrig.titanrig.repositories.PasswordRecoverRepository;
@@ -28,6 +31,9 @@ public class AuthService {
 
     @Value("${email.password-recover.uri}")
     private Long recoverUri;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private PasswordRecoverRepository passwordRecoverRepository;
@@ -68,6 +74,18 @@ public class AuthService {
         + recoverUri + token + ". Validade de " + tokenMinutes + " minutos";
 
         emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
+    }
+
+    @Transactional
+    public void saveNewPassword(NewPasswordDTO body) {
+        List<PasswordRecover> result = passwordRecoverRepository.searchValidTokens(body.getToken(), Instant.now());
+        if (result.size() == 0){
+            throw new ResourceNotFoundException("Token inválido");
+        }
+
+        User user = userRepository.findByEmail(result.get(0).getEmail());
+        user.setPassword(passwordEncoder.encode(body.getPassword()));
+        user = userRepository.save(user);
     }
 
     protected User authenticated() {
