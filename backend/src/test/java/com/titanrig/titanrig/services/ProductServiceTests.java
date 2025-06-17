202702f76accc3779b34477dd570818c44f02486
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-17 08:28:46 -0300

    Update Tests

--- backend/src/test/java/com/titanrig/titanrig/services/ProductServiceTests.java
+++ backend/src/test/java/com/titanrig/titanrig/services/ProductServiceTests.java
@@ -2,14 +2,6 @@ package com.titanrig.titanrig.services;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import com.titanrig.titanrig.dto.ProductDTO;
-import com.titanrig.titanrig.entities.Category;
-import com.titanrig.titanrig.entities.Product;
-import com.titanrig.titanrig.repositories.CategoryRepository;
-import com.titanrig.titanrig.repositories.ProductRepository;
-import com.titanrig.titanrig.tests.Factory;
-
 import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
@@ -29,6 +21,15 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.titanrig.titanrig.dto.ProductDTO;
+import com.titanrig.titanrig.entities.Category;
+import com.titanrig.titanrig.entities.Product;
+import com.titanrig.titanrig.repositories.CategoryRepository;
+import com.titanrig.titanrig.repositories.ProductRepository;
+import com.titanrig.titanrig.exceptions.ResourceNotFoundException;
+import com.titanrig.titanrig.tests.Factory;
+
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -74,5 +75,21 @@ public class ProductServiceTests {
         Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
         Mockito.when(categoryRepository.getReferenceById(existingId)).thenReturn(category);
         Mockito.when(categoryRepository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExist(){
+        
+        ProductDTO result = service.update(existingId, productDTO);
+
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist(){
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.update(nonExistingId, productDTO);
+        });
     }
 }
