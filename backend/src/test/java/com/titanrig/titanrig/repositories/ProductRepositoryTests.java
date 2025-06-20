3b207537eb49da0666b9893ba694936198aed201
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-20 12:30:39 -0300

    Test SaveShouldPersist

--- backend/src/test/java/com/titanrig/titanrig/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/titanrig/titanrig/repositories/ProductRepositoryTests.java
@@ -1,8 +1,14 @@
 package com.titanrig.titanrig.repositories;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.titanrig.titanrig.entities.Product;
+import com.titanrig.titanrig.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -19,5 +25,16 @@ public class ProductRepositoryTests {
         existingId = 1L;
         countTotalProducts = 25L;
         nonExistingId = 100L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull(){
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
     }
 }
