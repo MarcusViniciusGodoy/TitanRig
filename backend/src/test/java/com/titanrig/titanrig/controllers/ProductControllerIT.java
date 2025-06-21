96c534cf007114278050e2722822700aeff82ba7
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-21 07:41:11 -0300

    Test return product when visitor authenticated

--- backend/src/test/java/com/titanrig/titanrig/controllers/ProductControllerIT.java
+++ backend/src/test/java/com/titanrig/titanrig/controllers/ProductControllerIT.java
@@ -23,25 +23,27 @@ public class ProductControllerIT {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private TokenUtil tokenUtil;
 
 	private long existingId;
 	private long nonExistingId;
 	
-	private String operatorUsername;
-	private String operatorPassword;
-	private String adminUsername;
-	private String adminPassword;
-	
+	private String visitorUsername;
+	private String visitorPassword;
+	private String memberUsername;
+	private String memberPassword;
 	@BeforeEach
 	void setUp() throws Exception {
+
+		existingId = 1L;
+		nonExistingId = 100000L;
 		
-		operatorUsername = "ana@gmail.com";
-		operatorPassword = "123456";
-		adminUsername = "bob@gmail.com";
-		adminPassword = "123456";
+		visitorUsername = "bob@gmail.com";
+		visitorPassword = "123456";
+		memberUsername = "ana@gmail.com";
+		memberPassword = "123456";
 	}
 
 	@Test
@@ -52,6 +54,24 @@ public class ProductControllerIT {
 					.contentType(MediaType.APPLICATION_JSON));
 
 		result.andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	public void findByIdShouldReturnProductWhenUserVisitorAuthenticated() throws Exception {
+
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, visitorUsername, visitorPassword);
+		
+		ResultActions result =
+				mockMvc.perform(get("/products/{id}", existingId)
+					.header("Authorization", "Bearer " + accessToken)
+					.contentType(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(existingId));
+		result.andExpect(jsonPath("$.name").isNotEmpty());
+		result.andExpect(jsonPath("$.description").isNotEmpty());
+		result.andExpect(jsonPath("$.price").isNotEmpty());
+		result.andExpect(jsonPath("$.imgUrl").isNotEmpty());
 	}
 }
 
