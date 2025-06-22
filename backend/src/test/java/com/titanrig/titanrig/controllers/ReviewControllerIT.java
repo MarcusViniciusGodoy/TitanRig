7647947c84a802a4a6f2df2935bd2922fa9e1f40
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-22 17:11:18 -0300

    Test InsertShouldReturnUnauthorized

--- backend/src/test/java/com/titanrig/titanrig/controllers/ReviewControllerIT.java
+++ backend/src/test/java/com/titanrig/titanrig/controllers/ReviewControllerIT.java
@@ -1,13 +1,20 @@
 package com.titanrig.titanrig.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.titanrig.titanrig.dto.ReviewDTO;
 import com.titanrig.titanrig.tests.TokenUtil;
 
 @SpringBootTest
@@ -36,5 +43,23 @@ public class ReviewControllerIT {
 		visitorPassword = "123456";
 		memberUsername = "ana@gmail.com";
 		memberPassword = "123456";
+	}
+
+    @Test
+	public void insertShouldReturnUnauthorizedWhenNotValidToken() throws Exception {
+
+		ReviewDTO reviewDTO = new ReviewDTO();
+		reviewDTO.setText("Gostei do produto!");
+		reviewDTO.setProductId(1L);
+
+		String jsonBody = objectMapper.writeValueAsString(reviewDTO);
+		
+		ResultActions result =
+				mockMvc.perform(post("/reviews")
+						.content(jsonBody)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isUnauthorized());
 	}
 }
