14232c4b4443e9fed5f8502b06096b6a93bf9f4f
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-27 09:02:50 -0300

    Created ReviewTests

---
+++ backend/src/test/java/com/titanrig/titanrig/entities/ReviewTests.java
@@ -0,0 +1,22 @@
+package com.titanrig.titanrig.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ReviewTests {
+
+	@Test
+	public void reviewShouldHaveCorrectStructure() {
+	
+		Review entity = new Review();
+		entity.setId(1L);
+		entity.setText("Text");
+		entity.setProduct(new Product());
+		entity.setUser(new User());
+	
+		Assertions.assertNotNull(entity.getId());
+		Assertions.assertNotNull(entity.getText());
+		Assertions.assertNotNull(entity.getProduct());
+		Assertions.assertNotNull(entity.getUser());
+	}
+}
