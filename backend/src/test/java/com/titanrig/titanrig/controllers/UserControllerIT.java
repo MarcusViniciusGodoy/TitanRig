1cfc5a9c03f0a66c10ae26ae2f0db93698b848a7
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-30 10:43:44 -0300

    Tests FindMe on User

--- backend/src/test/java/com/titanrig/titanrig/controllers/UserControllerIT.java
+++ backend/src/test/java/com/titanrig/titanrig/controllers/UserControllerIT.java
@@ -1,5 +1,58 @@
 package com.titanrig.titanrig.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.titanrig.titanrig.tests.TokenUtil;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class UserControllerIT {
 
+     @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TokenUtil tokenUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String clientUsername;
+    private String clientPassword;
+    private String adminUsername;
+    private String adminPassword;
+
+    @BeforeEach
+    void setUp() {
+        clientUsername = "alex@gmail.com";
+        clientPassword = "123456";
+        adminUsername = "maria@gmail.com";
+        adminPassword = "123456";
+    }
+
+    @Test
+    public void findMeShouldReturnUserWhenClientAuthenticated() throws Exception {
+        String token = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+
+        ResultActions result = mockMvc.perform(get("/users/me")
+                .header("Authorization", "Bearer " + token)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.email").value(clientUsername));
+    }
 }
