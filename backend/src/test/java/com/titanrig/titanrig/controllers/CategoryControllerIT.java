4a003c8177c6033a10c922ccae1b6d4a6158d48f
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-04 08:17:06 -0300

    Test Category findByIdReturnNotFoundWhenIdDoesNotExist

--- backend/src/test/java/com/titanrig/titanrig/controllers/CategoryControllerIT.java
+++ backend/src/test/java/com/titanrig/titanrig/controllers/CategoryControllerIT.java
@@ -9,11 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,9 +19,6 @@ public class CategoryControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Test
     public void findByIdShouldReturnCategoryWhenIdExists() throws Exception {
@@ -36,4 +30,12 @@ public class CategoryControllerIT {
             .andExpect(jsonPath("$.id").value(existingId));
     }
     
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+        long nonExistingId = 9999L;
+
+        mockMvc.perform(get("/categories/{id}", nonExistingId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
 }
